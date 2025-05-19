f769474c87e92c1a13454bb46d4f2e713f94dfc7
Alex Rusnac <rusnac@gmail.com>
2025-05-19 18:15:11 +0300

    Add basic auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-security'],
+  security: {
+    enabled: true,
+    basicAuth: {
+      enabled: true,
+      include: ['/api/telemetry'],
+      name: process.env.BASIC_AUTH_USER || '',
+      pass: process.env.BASIC_AUTH_PASSWORD || '',
+      message: 'Authentication required!',
+    }
+  }
 })
